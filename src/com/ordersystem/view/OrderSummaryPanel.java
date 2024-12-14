81ee6e79bd9afe0fef81856266f7d3bd812099f9
henry22 <henry811010@gmail.com>
2024-12-14 19:50:55 +1100

    update OrderSystemUI & OrderSummaryPanel

--- src/com/ordersystem/view/OrderSummaryPanel.java
+++ src/com/ordersystem/view/OrderSummaryPanel.java
@@ -1,7 +1,28 @@
 package com.ordersystem.view;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.TitledBorder;
 
 public class OrderSummaryPanel extends JPanel {
-  
+  private final JLabel totalAmountLabel;
+  private final JLabel dailyRevenueLabel;
+
+  public OrderSummaryPanel() {
+    this.totalAmountLabel = new JLabel("總金額: $0.00");
+    this.dailyRevenueLabel = new JLabel("今日營業額: $0.00");
+
+    MatteBorder matteBorder = BorderFactory.createMatteBorder(2, 5, 2, 5, Color.GRAY);
+    TitledBorder title = BorderFactory.createTitledBorder(matteBorder, "訂單摘要");
+
+    title.setTitleJustification(TitledBorder.CENTER);
+    setBorder(title);
+
+    add(this.totalAmountLabel);
+    add(this.dailyRevenueLabel);
+  }
 }
--- src/com/ordersystem/view/OrderSystemUI.java
+++ src/com/ordersystem/view/OrderSystemUI.java
@@ -14,17 +14,29 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
 
 import java.awt.BorderLayout; // Java AWT(Abstract Window Toolkit)
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import com.ordersystem.controller.Consumer;
 import com.ordersystem.controller.OrderFileManager;
@@ -124,16 +136,25 @@ public class OrderSystemUI extends JFrame {
     // 建立主要面板
     // TODO: Finish the rest of the parts.
     this.menuPanel = createMenuPanel();
+    this.orderPanel = createOrderPanel();
+    this.statusPanel = createStatusPanel();
+
+    this.orderSummaryPanel = new OrderSummaryPanel();
 
     this.menuPanel.setBackground(java.awt.Color.GRAY);
 
     add(this.menuPanel, BorderLayout.NORTH);
+    add(this.orderPanel, BorderLayout.CENTER);
+    add(this.statusPanel, BorderLayout.SOUTH);
+    add(this.orderSummaryPanel, BorderLayout.EAST);
   }
 
   // 建立菜單面板
   private JPanel createMenuPanel() {
     JPanel panel = new JPanel(new GridLayout(1, Food.values().length, 15, 0));
-    panel.setBorder(BorderFactory.createTitledBorder("菜單"));
+    TitledBorder titledBorder = BorderFactory.createTitledBorder("菜單");
+    titledBorder.setTitleColor(Color.WHITE);
+    panel.setBorder(titledBorder);
 
     // 為每個餐點建立卡片
     for(Food food : Food.values()) {
@@ -197,7 +218,217 @@ public class OrderSystemUI extends JFrame {
     return panel;
   }
 
-  private void addListeners() {}
+  private JPanel createOrderPanel() {
+    JPanel panel = new JPanel();
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    panel.setBackground(Color.darkGray);
+
+    // 建立選擇區域
+    JPanel selectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    selectionPanel.setBorder(BorderFactory.createTitledBorder("選擇餐點"));
+
+    // 建立餐點選擇器
+    itemSelector = new JComboBox<>(menuItems.toArray(new MenuItem[0]));
+    // 建立數量選擇器
+    quantitySpinner = new JSpinner(new SpinnerNumberModel(1, 1, 99, 1));
+    // 建立下單按鈕
+    addButton = new JButton("下單");
+
+    // 將元件加入選擇面板中
+    selectionPanel.add(itemSelector);
+    selectionPanel.add(quantitySpinner);
+    selectionPanel.add(addButton);
+
+    // 建立購物車面板
+    JPanel cartPanel = new JPanel();
+    cartPanel.setLayout(new BoxLayout(cartPanel, BoxLayout.X_AXIS));
+
+    cartPanel.add(selectionPanel);
+
+    // 建立購物車清單面板(可捲動)
+    this.cartListPanel = new JPanel();
+    this.cartListPanel.setLayout(new BoxLayout(this.cartListPanel, BoxLayout.Y_AXIS));
+
+    // 建立捲動面板
+    JScrollPane scrollPane = new JScrollPane(this.cartListPanel);
+    scrollPane.setBorder(BorderFactory.createTitledBorder("目前訂單已選項目"));
+    scrollPane.setPreferredSize(new Dimension(400, 200));
+    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+    this.cartListPanel.setBorder(null);
+
+    cartPanel.add(scrollPane);
+    panel.add(cartPanel);
+
+    // 建立按鈕面板
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+    buttonPanel.setBackground(Color.darkGray);
+
+    // 建立送出和處理的按鈕
+    orderButton = new JButton("送出訂單");
+    processButton = new JButton("處理訂單");
+
+    buttonPanel.add(orderButton);
+    buttonPanel.add(processButton);
+
+    panel.add(buttonPanel);
+
+    return panel;
+  }
+
+  private JPanel createStatusPanel() {
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBorder(BorderFactory.createTitledBorder("訂單狀態"));
+
+    // 建立狀態顯示面板
+    this.statusDisplay = new JPanel(new GridLayout(1, 3, 10, 0));
+
+    // 等待狀態面板
+    JPanel waitingStatusPanel = new JPanel(new GridLayout(2, 1));
+    this.waitingStatusLabel = new JLabel("訂單已接收: 0");
+    this.waitingTimeLabel = new JLabel("更新時間: " + getCurrentTime());
+    waitingStatusPanel.add(this.waitingStatusLabel);
+    waitingStatusPanel.add(waitingTimeLabel);
+    waitingStatusPanel.setBorder(BorderFactory.createEtchedBorder());
+
+    // 處理狀態面板
+    JPanel processingStatusPanel = new JPanel(new GridLayout(2, 1));
+    this.processingStatusLabel = new JLabel("正在準備中: 0");
+    this.processingTimeLabel = new JLabel("更新時間: " + getCurrentTime());
+    processingStatusPanel.add(this.processingStatusLabel);
+    processingStatusPanel.add(processingTimeLabel);
+    processingStatusPanel.setBorder(BorderFactory.createEtchedBorder());
+
+    // 完成狀態面板
+    JPanel completedStatusPanel = new JPanel(new GridLayout(2, 1));
+    this.completedStatusLabel = new JLabel("餐點已完成: 0");
+    this.completedTimeLabel = new JLabel("更新時間: " + getCurrentTime());
+    completedStatusPanel.add(this.completedStatusLabel);
+    completedStatusPanel.add(this.completedTimeLabel);
+    completedStatusPanel.setBorder(BorderFactory.createEtchedBorder());
+
+    // 加入所有狀態面板
+    this.statusDisplay.add(waitingStatusPanel);
+    this.statusDisplay.add(processingStatusPanel);
+    this.statusDisplay.add(completedStatusPanel);
+
+    panel.add(this.statusDisplay, BorderLayout.CENTER);
+
+    return panel;
+  }
+
+  private String getCurrentTime() {
+    return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+  }
+
+  private void addListeners() {
+    // 新增餐攢按鈕監聽器
+    this.addButton.addActionListener(e -> {
+      MenuItem item = (MenuItem) itemSelector.getSelectedItem();
+      int quantity = (Integer) quantitySpinner.getValue();
+
+      // 建立新的購物車項目
+      JPanel newPanel = createCartItem(item, quantity);
+    });
+  }
+
+  private JPanel createCartItem(MenuItem item, int quantity) {
+    // 檢查餐點項目是否已存在在購物車中
+    if(this.cartListItems.containsKey(item)) {
+      // 更新已存在餐點的數量
+      int newQuantity = this.cartListItems.get(item) + quantity;
+      this.cartListItems.put(item, newQuantity);
+
+      // 更新購物車面板
+      for(Component comp : this.cartListPanel.getComponents()) {
+        if(comp instanceof JPanel) {
+          JPanel panel = (JPanel) comp;
+
+          for(Component panelComp : panel.getComponents()) {
+            if(panelComp instanceof JLabel) {
+              JLabel label = (JLabel) panelComp;
+
+              if(label.getText().equals(item.getName())) {
+                // 更新數量label
+                for(Component quantityComp : panel.getComponents()) {
+                  if(quantityComp instanceof JLabel) {
+                    JLabel quantityLabel = (JLabel) quantityComp;
+
+                    if(quantityLabel.getText().startsWith("x ")) {
+                      quantityLabel.setText("x " + newQuantity);
+                      break;
+                    }
+                  }
+                }
+
+                return null;
+              }
+            }
+          }
+        }
+      }
+    } else {
+      this.cartListItems.put(item, quantity);
+    }
+
+    JPanel cartItem = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    cartItem.setMaximumSize(new Dimension(Integer.MIN_VALUE, 50));
+    cartItem.setPreferredSize(new Dimension(380, 50));
+
+    JLabel namLabel = new JLabel(item.getName());
+    JLabel quantityLabel = new JLabel("x " + this.cartListItems.get(item));
+
+    RoundLabel imgLabel = new RoundLabel("IMG");
+    imgLabel.setPreferredSize(new Dimension(40, 40));
+    imgLabel.setOpaque(false);
+    imgLabel.setBackground(Color.LIGHT_GRAY);
+
+    JButton deleteButton = new JButton("移除");
+    deleteButton.setFocusPainted(false);
+    deleteButton.setContentAreaFilled(false);
+    deleteButton.setPreferredSize(new Dimension(50, 20));
+
+    deleteButton.addActionListener(e -> {
+      cartListItems.remove(item);
+      cartListPanel.remove(cartItem);
+      forceUpdateUI();
+    });
+  }
 
   private void loadSavedOrders() {}
+
+  // 強制更新 UI 的方法
+  private void forceUpdateUI() {
+    // 更新購物車面板
+    this.cartListPanel.revalidate();
+    this.cartListPanel.repaint();
+
+    // 更新整個視窗
+    revalidate();
+    repaint();
+  }
+
+  private class RoundLabel extends JLabel {
+    public RoundLabel(String text) {
+      super(text);
+      setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+      Graphics2D g2 = (Graphics2D) g.create();
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+      // 畫背景
+      g2.setColor(getBackground());
+      g2.fillOval(0, 0, getWidth() - 1, getHeight() - 1);
+
+      // 畫文字
+      g2.setColor(getForeground());
+      super.paintComponent(g2);
+      g2.dispose();
+    }
+  }
 }
